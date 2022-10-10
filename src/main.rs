c9e212ce15c43f998968fec06fa7b325fee03110
Samuel Craig Carey <sccarey@sandia.gov>
2022-10-10 15:39:40 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use futuresdr::{
+    anyhow::Result,
+    async_io::block_on,
+    async_trait::async_trait,
+    runtime::{
+        Block, BlockMeta, BlockMetaBuilder, Flowgraph, Kernel, MessageIo, MessageIoBuilder,
+        Runtime, StreamIo, StreamIoBuilder, WorkIo,
+    },
+};
+
+fn main() {
+    let mut fg = Flowgraph::new();
+    fg.add_block(ExampleBlock.into());
+    let (task, mut handle) = block_on(Runtime::new().start(fg));
+
+    block_on(handle.terminate()).unwrap();
+    block_on(task).unwrap();
+}
+
+struct ExampleBlock;
+
+impl Into<Block> for ExampleBlock {
+    fn into(self) -> Block {
+        Block::new(
+            BlockMetaBuilder::new("ExampleBlock").build(),
+            StreamIoBuilder::new().build(),
+            MessageIoBuilder::new().build(),
+            self,
+        )
+    }
+}
+
+#[async_trait]
+impl Kernel for ExampleBlock {
+    async fn work(
+        &mut self,
+        _io: &mut WorkIo,
+        _sio: &mut StreamIo,
+        _mio: &mut MessageIo<Self>,
+        _meta: &mut BlockMeta,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    async fn deinit(
+        &mut self,
+        _s: &mut StreamIo,
+        _m: &mut MessageIo<Self>,
+        _b: &mut BlockMeta,
+    ) -> Result<()> {
+        println!("Drop from Kernel");
+        Ok(())
+    }
+}
+
+impl Drop for ExampleBlock {
+    fn drop(&mut self) {
+        println!("Drop from Drop");
+    }
+}
